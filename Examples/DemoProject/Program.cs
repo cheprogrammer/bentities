5876930e8998e666c976554c782764e788438e5e
Denis Goriachev <cheprogrammer@gmail.com>
2018-03-27 16:03:38 +0300

    Added missed files

--- Examples/DemoProject/Program.cs
+++ Examples/DemoProject/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BEntities;
+using BEntities.Components;
+using DemoProject.Components;
+using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +14,22 @@ namespace DemoProject
 	{
 		static void Main(string[] args)
 		{
+			// fisrt of all, perform scanning of assemblies for factory
+			ECSManagerFactory.ScanAssemblies(AppDomain.CurrentDomain.GetAssemblies());
 
+			// after that, we can create ECS Manager
+			ECSManager manager = ECSManagerFactory.CreateECSManager();
+
+			// let's create first entity!
+			Entity firstEntity = manager.CreateEntity();
+
+			// now let's get transform component and move entity a little bit
+			Transform2DComponent component = firstEntity.GetComponent<Transform2DComponent>();
+			component.Position += new Vector2(10, 10);
+
+			// attach a new component and change its values
+			TestComponent newComponent = firstEntity.AttachComponent<TestComponent>();
+			newComponent.Variable1 += 10;
 		}
 	}
 }
--- Examples/DemoProject/Systems/TestComponentSystem.cs
+++ Examples/DemoProject/Systems/TestComponentSystem.cs
@@ -1,4 +1,6 @@
 ﻿using BEntities;
+using DemoProject.Components;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +9,16 @@ using System.Threading.Tasks;
 
 namespace DemoProject.Systems
 {
+	[ComponentSystem(SystemProcessingType.Update, 1, typeof(TestComponent))]
 	public class TestComponentSystem : BaseComponentProcessingSystem
 	{
-		protected override  
+		protected override void ProcessComponent(GameTime gameTime, BaseComponent component)
+		{
+			// do something with component
+			TestComponent testComponent = (TestComponent)component;
+
+			testComponent.Variable1 += 0;
+			testComponent.Variable2 += "";
+		}
 	}
 }
