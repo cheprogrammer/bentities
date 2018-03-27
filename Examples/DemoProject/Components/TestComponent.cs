cad66f3cb66e41964fbe56058d03a55a023b3c18
Denis Goriachev <cheprogrammer@gmail.com>
2018-03-27 16:03:15 +0300

    Changes structure of project Added Demo project

---
+++ Examples/DemoProject/Components/TestComponent.cs
@@ -0,0 +1,16 @@
+﻿using BEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject.Components
+{
+	public class TestComponent : BaseComponent
+	{
+		public int Variable1 { get; set; }
+
+		public string Variable2 { get; set; }
+	}
+}
---
+++ Examples/DemoProject/Systems/TestComponentSystem.cs
@@ -0,0 +1,14 @@
+﻿using BEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject.Systems
+{
+	public class TestComponentSystem : BaseComponentProcessingSystem
+	{
+		protected override  
+	}
+}
